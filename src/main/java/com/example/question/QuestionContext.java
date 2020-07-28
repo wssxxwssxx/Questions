2269fa3a1b0e80dfa4de6f4501924c9ba25695cd
Stranger09 <stranger1995.ay@gmail.com>
2020-07-28 20:33:08 +0300

    Initial commit

---
+++ src/main/java/com/example/question/QuestionContext.java
@@ -0,0 +1,7 @@
+package com.example.question;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuestionContext {
+}
---
+++ src/main/java/com/example/question/QuestionController.java
@@ -0,0 +1,7 @@
+package com.example.question;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class QuestionController {
+}
