aab5135a41e63f82a2e00cec2a00ba6014e3dfbc
Stranger09 <stranger1995.ay@gmail.com>
2020-07-28 22:33:49 +0300

    fix

--- src/main/java/com/example/question/config/SpringConfig.java
+++ src/main/java/com/example/question/config/SpringConfig.java
@@ -1,71 +1,10 @@
-
 package com.example.question.config;
 
+import org.springframework.stereotype.Controller;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+@Controller
+public class SpringConfig  {
 
-
-import org.springframework.http.converter.AbstractHttpMessageConverter;
-import org.springframework.http.converter.FormHttpMessageConverter;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.*;
-
-import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
-
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-
-@Configuration
-@ComponentScan
-@EnableWebMvc
-public class SpringConfig implements WebMvcConfigurer  {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/js/**")
-                .addResourceLocations("classpath:/static/js/");
-        registry
-                .addResourceHandler("/css/**")
-                .addResourceLocations("classpath:/static/css/");
-        registry
-                .addResourceHandler("/fonts/**")
-                .addResourceLocations("classpath:/static/fonts/");
-    }
-
-
-    @Bean
-    public FreeMarkerViewResolver viewResolver() {
-        FreeMarkerViewResolver viewResolver = new FreeMarkerViewResolver();
-        viewResolver.setContentType("text/html;charset=UTF-8");
-        viewResolver.setPrefix("templates/");
-        viewResolver.setSuffix(".ftlh");
-        return viewResolver;
-    }
-
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.freeMarker();
-        registry.viewResolver(viewResolver());
-    }
-
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.forEach(converter -> {
-            if (converter instanceof AbstractHttpMessageConverter) {
-                ((AbstractHttpMessageConverter) converter).setDefaultCharset(StandardCharsets.UTF_8);
-                if (converter instanceof StringHttpMessageConverter) {
-                    ((StringHttpMessageConverter) converter).setWriteAcceptCharset(false);
-                }
-            } else if (converter instanceof FormHttpMessageConverter) {
-                ((FormHttpMessageConverter) converter).setCharset(StandardCharsets.UTF_8);
-                ((FormHttpMessageConverter) converter).setMultipartCharset(StandardCharsets.UTF_8);
-            }
-        });
-    }
 }
 
 
