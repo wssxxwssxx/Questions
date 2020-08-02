848f7cc5e3bcd2c7d3f6ca27f309d76f51f40759
Stranger09 <stranger1995.ay@gmail.com>
2020-08-02 23:13:29 +0300

    little refactor

--- src/main/java/com/example/question/entity/User.java
+++ src/main/java/com/example/question/entity/User.java
@@ -1,5 +1,6 @@
 package com.example.question.entity;
 
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -8,35 +9,28 @@ import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.Set;
 
+@Data
 @Entity
 @Table(name = "t_user")
 public class User implements UserDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Size(min=2, message = "Не меньше 5 знаков")
     private String username;
+
     @Size(min=2, message = "Не меньше 5 знаков")
     private String password;
+
     @Transient
     private String passwordConfirm;
+
     @ManyToMany(fetch = FetchType.EAGER)
     private Set<Role> roles;
 
     public User() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
     }
 
     @Override
@@ -57,10 +51,6 @@ public class User implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     @Override
@@ -88,9 +78,4 @@ public class User implements UserDetails {
     public Set<Role> getRoles() {
         return roles;
     }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
 }
--- src/main/java/com/example/question/repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.question.repository;
-/**
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
-*/
