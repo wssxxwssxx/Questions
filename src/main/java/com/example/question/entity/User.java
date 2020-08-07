9f93e3337d30d1384aa0d2db64553a6fedf45abe
vlad3122009 <vlad3122009@gmail.com>
2020-08-07 23:41:34 +0300

    контроллер, и мелочи

--- src/main/java/com/example/question/entity/User.java
+++ src/main/java/com/example/question/entity/User.java
@@ -1,12 +1,13 @@
 package com.example.question.entity;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+
 import java.util.Collection;
 import java.util.Set;
 
